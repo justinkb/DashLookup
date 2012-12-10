bffd8beff229b39ccea4374e0fee5c41f3d4e3f6
Paul Mulders <justinkb@gmail.com>
2012-12-11 00:33:56 +0100

    cleanup imports

--- src/dashlookup/handlers/DashLookupHandler.java
+++ src/dashlookup/handlers/DashLookupHandler.java
@@ -4,12 +4,9 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.swt.program.Program;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.texteditor.ITextEditor;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 
