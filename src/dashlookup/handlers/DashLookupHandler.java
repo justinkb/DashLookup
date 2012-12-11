ae913f5a09093afe19dd6c2a18eedde89330a116
Paul Mulders <justinkb@gmail.com>
2012-12-11 01:26:05 +0100

    remove .settings from git repo, cleanup

--- src/dashlookup/handlers/DashLookupHandler.java
+++ src/dashlookup/handlers/DashLookupHandler.java
@@ -26,6 +26,7 @@ public class DashLookupHandler extends AbstractHandler {
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
+	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		ISelectionService service = window.getSelectionService();
