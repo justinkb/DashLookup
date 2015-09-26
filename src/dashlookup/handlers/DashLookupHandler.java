9eebe75f5e78ff68624ed952105f3837378e07ac
Toomas Uudisaru <toomas.uudisaru@gmail.com>
2015-09-26 15:11:16 +0300

    Fix for the text selection in Eclipse Mars.
    The selected text in text editor is not correctly looked up in
    Eclipse Mars.
    Add alternative selection lookup from the active page (see
    http://wiki.eclipse.org/FAQ_How_do_I_find_out_what_object_is_selected%3F)

--- src/dashlookup/handlers/DashLookupHandler.java
+++ src/dashlookup/handlers/DashLookupHandler.java
@@ -3,12 +3,14 @@ package dashlookup.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.swt.program.Program;
-import org.eclipse.ui.ISelectionService;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.program.Program;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -29,16 +31,38 @@ public class DashLookupHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		ISelectionService service = window.getSelectionService();
 
-		if (service != null) {
-			ISelection selection = service.getSelection();
-			if (selection instanceof ITextSelection) {
-				String selectedText = ((ITextSelection) selection).getText();
-				if (!selectedText.isEmpty())
-					Program.launch("dash://" + selectedText);
-			}
+		String selectedText = getSelectedText(window);
+		
+		if (selectedText.isEmpty() && window.getActivePage() != null) {
+			selectedText = getTextFromSelection(window.getActivePage().getSelection());
 		}
+		
+		if (!selectedText.isEmpty()) {
+			Program.launch("dash://" + selectedText);
+		}
+		
 		return null;
 	}
+	
+	private String getTextFromSelection(ISelection selection) {
+		String selectedText = "";
+		if (selection instanceof ITextSelection) {
+			selectedText = ((ITextSelection) selection).getText();
+		}
+		
+		return selectedText;
+	}
+
+	private String getSelectedText(IWorkbenchWindow window) {
+		String selectedText = "";
+		final ISelectionService service = window.getSelectionService();
+
+		if (service != null) {
+			selectedText = getTextFromSelection(service.getSelection());
+		}
+		
+		return selectedText;
+	}
+
 }
